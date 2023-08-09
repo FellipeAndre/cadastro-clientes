4b0be7f253b95b7f8458e4e37d62ac15cefccb64
FellipeAndre <felipetobiasandre.jc@gmail.com>
2023-08-08 23:05:09 -0300

    Criação do método para add idCliente nas tabelas relacioandas

--- cadastro/src/main/java/br/com/cadastro/cadastro/serviceImpl/ClienteServiceImpl.java
+++ cadastro/src/main/java/br/com/cadastro/cadastro/serviceImpl/ClienteServiceImpl.java
@@ -4,7 +4,9 @@ import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.cadastro.cadastro.entity.Cliente;
 import br.com.cadastro.cadastro.entity.Endereco;
@@ -23,36 +25,48 @@ public class ClienteServiceImpl implements ClienteService {
 
 	@Autowired
 	private ClienteRepositoryImpl repositoryCliente;
-	
+
 	@Autowired
 	private EnderecoRepositoryImpl enderecoRepository;
-	
+
 	@Autowired
 	private TelefoneRepositoryImpl telefoneRepository;
-	
 
 	@Override
 	public Cliente save(Cliente cliente) {
-			
-		  Cliente clienteSalvo = new Cliente();
-			Endereco enderecoSalvo = this.enderecoRepository
-					.save(cliente.getIdEndereco());
-			
-			Telefone telSalvo = this.telefoneRepository.save(cliente.getIdTelefone());
-			
-			if((enderecoSalvo.getIdEndereco() != null || Objects.nonNull(enderecoSalvo))
-					&& (telSalvo.getIdTelefone() != null || Objects.nonNull(telSalvo))) {
-				
-				cliente.setIdEndereco(enderecoSalvo);
-				cliente.setIdTelefone(telSalvo);
-				
-				clienteSalvo = this.repositoryCliente.save(cliente);
-			}
-			
+
+		Cliente clienteSalvo = new Cliente();
+		Endereco enderecoSalvo = this.enderecoRepository.save(cliente.getIdEndereco());
+
+		Telefone telSalvo = this.telefoneRepository.save(cliente.getIdTelefone());
+
+		if ((enderecoSalvo.getIdEndereco() != null || Objects.nonNull(enderecoSalvo))
+				&& (telSalvo.getIdTelefone() != null || Objects.nonNull(telSalvo))) {
+
+			cliente.setIdEndereco(enderecoSalvo);
+			cliente.setIdTelefone(telSalvo);
+
+			clienteSalvo = this.repositoryCliente.save(cliente);
+			this.salvarIdCliente(clienteSalvo);
+		}
+
 		return clienteSalvo;
 	}
-	
-	
+
+	private void salvarIdCliente(Cliente cliente) {
+
+		this.repositoryCliente.findById(cliente.getIdCliente()).map(c -> {
+
+			c.getIdEndereco().setId_Cliente(cliente);
+			c.getIdTelefone().setId_cliente(cliente);
+
+			this.enderecoRepository.save(c.getIdEndereco());
+			this.telefoneRepository.save(c.getIdTelefone());
+
+			return cliente;
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+
 	@Override
 	public Cliente findByIdCliente(Integer id) {
 
@@ -66,8 +80,5 @@ public class ClienteServiceImpl implements ClienteService {
 
 		return clienteEncontrado;
 	}
-
-
-
 
 }
